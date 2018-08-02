8e8d1d13a4c3da8dfc70456d8a18060638de8c56
Ricardo van Burik <ricardovanburik@hotmail.com>
2018-08-02 17:56:50 +0200

    New chapture(2), finished the Hello app and the Hobby Tutorial

---
+++ Hoofdstuk 2/Hello/Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Ricardo van Burik on 02-08-18.
+//  Copyright © 2018 Ricardo van Burik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
