49f70ad4c3656b1cb4303efa8af15e42450f7c00
Ricardo van Burik <ricardovanburik@hotmail.com>
2018-08-02 18:16:15 +0200

    First part of the TapMe app completed

---
+++ Hoofdstuk 2/CommonInputGroups/CommonInputGroups/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CommonInputGroups
+//
+//  Created by Ricardo van Burik on 02-08-18.
+//  Copyright © 2018 Ricardo van Burik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Button was tapped!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+}
+
